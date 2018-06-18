96a526b85cecc150047c4c29e131c7128bb8c1c3
Layne Faler <laynefaler@gmail.com>
2018-06-18 16:54:01 -0700

    Updates to actions for documentation

--- actions/index.js
+++ actions/index.js
@@ -6,15 +6,23 @@ var aliasCache = fs.readdirSync(path.join(__dirname, 'alias')).reduce(function (
     return acc;
 }, {});
 
+// format commands for CLI naming struture
+const formatCommands = (str) => {
+    return str;
+};
+
+// methods for CLI
 var index = fs.readdirSync(path.join(__dirname, 'methods')).reduce(function (acc, item) {
     // Import aliases with attached functionality
     if (!!aliasCache[item]) {
             aliasCache[item].forEach((alias) => {
-            acc[alias] = require(path.join(__dirname, 'methods', item));
+            acc[formatCommands(alias)] = require(path.join(__dirname, 'methods', item));
         });
     }
-    acc[item] = require(path.join(__dirname, 'methods', item));
+    // Import original command with attached functionality
+    acc[formatCommands(item)] = require(path.join(__dirname, 'methods', item));
     return acc;
 }, {});
 
+// All keys or command names for commander of CLI and values are the the functionality called
 module.exports = index;
