c0f9e3c157b65beb85874bdaa34359e2dbe9c615
Layne Faler <laynefaler@gmail.com>
2018-06-18 16:51:12 -0700

    Updates to actions

--- actions/index.js
+++ actions/index.js
@@ -1,7 +1,18 @@
 var fs = require('fs');
 var path = require('path');
 
+var aliasCache = fs.readdirSync(path.join(__dirname, 'alias')).reduce(function (acc, item) {
+    acc[item] = require(path.join(__dirname, 'alias', item));
+    return acc;
+}, {});
+
 var index = fs.readdirSync(path.join(__dirname, 'methods')).reduce(function (acc, item) {
+    // Import aliases with attached functionality
+    if (!!aliasCache[item]) {
+            aliasCache[item].forEach((alias) => {
+            acc[alias] = require(path.join(__dirname, 'methods', item));
+        });
+    }
     acc[item] = require(path.join(__dirname, 'methods', item));
     return acc;
 }, {});
