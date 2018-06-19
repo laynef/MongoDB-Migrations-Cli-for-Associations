3e6ed84041096a5c0b7c6f4b166ff7a98fe91e53
Layne Faler <laynefaler@gmail.com>
2018-06-19 14:46:51 -0700

    Updates to utils

--- utils/methods/copyAndPaste.js
+++ utils/methods/copyAndPaste.js
@@ -3,17 +3,17 @@ var ncp = require('ncp').ncp;
 
 module.exports = {
 
-    copy: function(destintionPath: String, preexistingPath: String, callback: Function) {
+    copy: function (destintionPath, preexistingPath, callback) {
         ncp(destintionPath, preexistingPath, { stopOnErr: true }, function (err) {
             if (err) console.error('');
             else callback();
         });
     },
-    mkdir: function(destintionPath: String) {
-        fs.mkdir(destintionPath, 0o777, function(error) {
+    mkdir: function (destintionPath) {
+        fs.mkdir(destintionPath, 0o777, function (error) {
             if (err) console.error('');
             else callback();
-        })
-    }
+        });
+    },
 
 };
