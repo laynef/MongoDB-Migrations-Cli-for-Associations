996ee8794d1a8b25124acc49a5d0dd3b7b372ddf
Layne Faler <laynefaler@gmail.com>
2018-06-19 15:26:22 -0700

    Updates to actions and aliases for documentation

--- actions/methods/documentation.js
+++ actions/methods/documentation.js
@@ -4,10 +4,18 @@ var utils = require('../../utils');
 
 var shortHandCommands = utils.shortHandCommands;
 
+var aliasCache = fs.readdirSync(path.join(__dirname, '..', 'alias')).map(e => e.replace(RegExp('.js', 'g'), '')).reduce(function (acc, item) {
+    acc[item] = require(path.join(__dirname, 'alias', item));
+    return acc;
+}, {});
+
 var keyMapOfCommands = fs.readdirSync(path.join(__dirname)).map(function (e) { return e.replace(RegExp('.js', 'g'), ''); }).filter(function (e) { return e !== 'documentation'; }).reduce(function (accumulation, file) {
     var shortcut = shortHandCommands(file);
     accumulation[file] = require(path.join(__dirname, file));
     accumulation[shortcut] = require(path.join(__dirname, file));
+    aliasCache[file].forEach(function (item) {
+        accumulation[item] = require(path.join(__dirname, file));
+    });
     return accumulation;
 }, {});
 
@@ -23,6 +31,7 @@ var command = function () {
 
     console.info('List of commands:\n');
     console.info('=> documentation - ' + description);
+    console.info('=> docs - ' + description);
     console.info('=> d - ' + description);
     for (var command in keyMapOfCommands) {
         console.info('=> ' + command + ' - ' + keyMapOfCommands[command].description);
