ee2f97837b728e4050f1f4f5488238a92cecfd12
Layne Faler <laynefaler@gmail.com>
2018-06-19 12:19:27 -0700

    Updates to methods of the two functions needed

--- actions/methods/columnUp.js
+++ actions/methods/columnUp.js
@@ -1,3 +1,11 @@
 module.exports = {
 
+    command: function () {
+
+    },
+
+    documentation: function () {
+
+    },
+
 };
--- actions/methods/unmigrate.js
+++ actions/methods/unmigrate.js
@@ -1,5 +1,13 @@
 // Added warning to not use on production (NODE_ENV === 'production' throw Error)
 
 module.exports = {
-    
+
+    command: function () {
+
+    },
+
+    documentation: function () {
+
+    },
+
 };
